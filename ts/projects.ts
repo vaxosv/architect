de7ca24e4da92688d78782c4f60f38d80ddbacff
vaxosv <vaxo.sv@gmail.com>
2020-04-09 21:54:47 +0400

    test commit 2

--- ts/projects.ts
+++ ts/projects.ts
@@ -1,21 +1,35 @@
-import {ellipsizeTextBox} from "./helpers/helper";
-
 export class Projects {
     showState = true;
 
     constructor() {
         this.setPositions()
+        this.events()
     }
 
     setPositions() {
-        $('.filter-head').each((i, el) => {
-            const element = $(el)
-            const pos = element.position().left
+        $('.filter-head').each(function (i, el) {
+            const width = $(this).innerWidth();
+            const leftSide = $(this).position().left;
+            const rightSide = window.innerWidth - (leftSide + width);
+            const minSide = Math.min(leftSide, rightSide);
+            $(this).find('.list-container')
+                .width(minSide + width)
+                .css({
+                    paddingLeft: leftSide - (840 / 2) + (width / 2)
+                });
+        })
+    }
 
-            // const someContainerWidth = element.children().first().children().first().children().first()
-            element.children().first().children().first().css({
-                paddingLeft: pos - (840 / 2) + (element.width() / 2) + 4
-            })
+    events() {
+        const filters = $('.filter-head');
+        const cover = $('.cover');
+
+        filters.hover(() => {
+            cover.addClass('full')
+        })
+
+        filters.mouseleave(() => {
+            cover.removeClass('full')
         })
     }
 }
