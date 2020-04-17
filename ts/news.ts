b436a8653d92658d96f901aca3bfee8724f73804
vaxosv <vaxo.sv@gmail.com>
2020-04-17 16:44:53 +0400

    details page css

--- ts/news.ts
+++ ts/news.ts
@@ -16,23 +16,10 @@ export class News {
 
     private eventListeners() {
 
-        $('.readMore').on('click', (event) => {
+        $('.readMore').on('click', function (event) {
             const element = $(event.target).parent()
             const elementText = element.data('text');
             element.html(elementText)
-
-
-            // element.parent().parent().parent().parent().siblings().each((i, el) => {
-            //     const element =
-            //         $(el).children('a')
-            //         .children('.postContainer')
-            //         .children('.description')
-            //         .children('.newsDescription')
-            //
-            //     const elementText = element.data('text');
-            //     ellipsizeTextBox(element, elementText, 230, false);
-            // });
-
         })
     }
 }
